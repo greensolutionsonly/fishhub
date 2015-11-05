bebc24a63eb2351986ec6dc5b7485acb9d0ba04c
Rama moorthy <rama.moorthy@razerzone.com>
2015-11-05 03:51:48 +0000

    refactor role

--- backend/role/role.go
+++ backend/role/role.go
@@ -1,8 +1,5 @@
 package role
 
-import (
-  "errors"
-)
 type RoleType uint8
 
 const (
@@ -15,34 +12,4 @@ const (
 
 type Role struct {
 	role RoleType
-}
-
-func GetRole(name string) *Role, error {
-role := Role{} 
-}
-func (r *Role) setRole(name string) error{
-	switch name {
-	case "wholesaler":
-    r.role= WholeSaler
-    return nil
-  case "retailer":
-    r.role = Retailor
-    return nil
-  case "collector":
-    r.role = Collector
-    return nil
-  case "fisher":
-    r.role = Fisher
-    return nil
-  case "admin":
-      return nil
-	}
-  return errors.New("Unknown Role Error")
-}
-
-func (r *Role) isAdmin() bool {
-	if r.Type == Admin {
-		return true
-	}
-	return false
 }
